38ba68ce4b87cb54e659319e8628619257f9eecb
SSSanga <ksa901125@gmail.com>
2023-08-28 18:18:31 +0900

    SA_push: quest 오후

---
+++ codes/MLs/Classifications/BreastCancerWisconsin_Regression_quest.py
@@ -0,0 +1,12 @@
+import pickle
+# 설명변수: 'texture_worst','smoothness_worst','concavity_worst'
+texture = float(input('texture_worst: '))
+smoothness = float(input('smoothness_worst: '))
+concavity = float(input('concavity_worst: '))
+
+with open ('datasets/BreastCancerWisconsin_Regression_quest.pkl', 'rb') as logistic :
+    loaded_model = pickle.load(logistic)
+    input_features = [[texture, smoothness, concavity]]
+    result_predict = loaded_model.predict(input_features)
+    print ('Predicted diagnosis : {}'.format(result_predict))
+    pass
